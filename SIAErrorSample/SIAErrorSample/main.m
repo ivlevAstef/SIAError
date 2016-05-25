3c356bc030843b3a34e12c73efa0c148a7da3bc4
ivlevAstef <ivlev.stef@gmail.com>
2016-05-25 21:09:17 +0600

    Sample code cleaned.

--- SIAErrorSample/SIAErrorSample/main.m
+++ SIAErrorSample/SIAErrorSample/main.m
@@ -29,58 +29,76 @@
 
 @end
 
+id sampleSubscibeAndReturnUnsubscribeObserver() {
+  SampleObserver* mainTarget = [[SampleObserver alloc] init];
+  
+  NSArray* validErrorCodes = @[[SIAErrorCodes OtherError1], [SIAErrorCodes OtherError2], [SIAErrorCodes MainError]];
+  [SIAErrorManager subscibeToArray:validErrorCodes Target:mainTarget Callback:^BOOL(SIAError* _Nonnull error) {
+    NSLog(@"Main Receive callback error: %@", [error description]);
+    return FALSE;
+  }];
+  
+  [SIAErrorManager subscibeToOne:[SIAErrorCodes MainError] Target:mainTarget Selector:@selector(sampleMainSelector:)];
+  
+  NSArray* validErrorCodes2 = @[[SIAErrorCodes OtherError2], [SIAErrorCodes MainError]];
+  return [SIAErrorManager subscibeToArray:validErrorCodes2 Target:mainTarget Selector:@selector(sampleMainSelector:)];
+}
+
+void sampleSubscribe() {
+  SampleObserver* target = [[SampleObserver alloc] init];
+  
+  NSArray* validErrorCodes = @[[SIAErrorCodes OtherError1], [SIAErrorCodes OtherError2]];
+  [SIAErrorManager subscibeToArray:validErrorCodes Target:target Callback:^BOOL(SIAError* _Nonnull error) {
+    NSLog(@"Receive callback error: %@", [error description]);
+    return FALSE;
+  }];
+  
+  [SIAErrorManager subscibeToOne:[SIAErrorCodes OtherError2] Target:target Selector:@selector(sampleSelector:)];
+  
+  NSArray* validErrorCodes2 = @[[SIAErrorCodes OtherError1], [SIAErrorCodes OtherError2], [SIAErrorCodes MainError]];
+  [SIAErrorManager subscibeToArray:validErrorCodes2 Target:target Callback:^BOOL(SIAError* _Nonnull error) {
+    NSLog(@"No Show because last method return TRUE.");
+    return FALSE;
+  }];
+}
+
+void sampleCallNotify() {
+  SIAErrorCollection* collection = [SIAErrorThread toCollection];
+  
+  NSLog(@"Notify all");
+  [SIAErrorManager notifyAll:collection];
+  NSLog(@"Notify max priority");
+  [SIAErrorManager notifyMaxPriority:collection];
+  
+  //no notify because 'toCollection' clean thread errors.
+  [SIAErrorManager notifyAll:[SIAErrorThread toCollection]];
+
+}
+
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
-    SampleObserver* mainTarget = [[SampleObserver alloc] init];
-    [SIAErrorManager subscibeToArray:@[[SIAErrorCodes OtherError1], [SIAErrorCodes OtherError2], [SIAErrorCodes MainError]] Target:mainTarget Callback:^BOOL(SIAError* _Nonnull error) {
-      NSLog(@"Main Receive callback error: %@", [error description]);
-      return FALSE;
-    }];
-    
-    [SIAErrorManager subscibeToOne:[SIAErrorCodes MainError] Target:mainTarget Selector:@selector(sampleMainSelector:)];
-    __block id observer = [SIAErrorManager subscibeToArray:@[[SIAErrorCodes OtherError2], [SIAErrorCodes MainError]] Target:mainTarget Selector:@selector(sampleMainSelector:)];
-    
+    id observer = sampleSubscibeAndReturnUnsubscribeObserver();
     __block SIAErrorCollection* mainCollection = nil;
     
     NSOperationQueue* queue = [NSOperationQueue new];
     [queue addOperationWithBlock:^{
-        SampleObserver* target = [[SampleObserver alloc] init];
-        [SIAErrorManager subscibeToArray:@[[SIAErrorCodes OtherError1], [SIAErrorCodes OtherError2]] Target:target Callback:^BOOL(SIAError* _Nonnull error) {
-          NSLog(@"Receive callback error: %@", [error description]);
-          return FALSE;
-        }];
-        
-        [SIAErrorManager subscibeToArray:@[[SIAErrorCodes OtherError1], [SIAErrorCodes OtherError2]] Target:target Selector:@selector(sampleSelector:)];
-        
-        [SIAErrorManager subscibeToArray:@[[SIAErrorCodes OtherError1], [SIAErrorCodes OtherError2], [SIAErrorCodes MainError]] Target:target Callback:^BOOL(SIAError* _Nonnull error) {
-          NSLog(@"No Show because last method return TRUE.");
-          return FALSE;
-        }];
-        
-        [SIAErrorThread addError:[SIAError createByCode:[SIAErrorCodes OtherError1]]];
-        [SIAErrorThread addError:[SIAError createByCode:[SIAErrorCodes OtherError2]]];
-        
-        
-        SIAErrorCollection* collection = [SIAErrorThread toCollection];
-        
-        NSLog(@"Notify all");
-        [SIAErrorManager notifyAll:collection];
-        NSLog(@"Notify max priority");
-        [SIAErrorManager notifyMaxPriority:collection];
-        
-        //no notify because 'toCollection' clean error thread errors.
-        [SIAErrorManager notifyAll:[SIAErrorThread toCollection]];
-        
-        [SIAErrorThread addError:[SIAError createByCode:[SIAErrorCodes MainError]]];
-        [SIAErrorThread addError:[SIAError createByCode:[SIAErrorCodes OtherError2]]];
-        mainCollection = [SIAErrorThread toCollection];
-        
+      sampleSubscribe();
       
+      [SIAErrorThread addError:[SIAError createByCode:[SIAErrorCodes OtherError1]]];
+      [SIAErrorThread addError:[SIAError createByCode:[SIAErrorCodes OtherError2]]];
+      
+      sampleCallNotify();
+      
+      [SIAErrorThread addError:[SIAError createByCode:[SIAErrorCodes MainError]]];
+      [SIAErrorThread addError:[SIAError createByCode:[SIAErrorCodes OtherError2]]];
+      
+      mainCollection = [SIAErrorThread toCollection];
     }];
     
     [queue waitUntilAllOperationsAreFinished];
     
     [SIAErrorManager unsubscribe:observer];
+    
     NSLog(@"Notify Main max priority");
     [SIAErrorManager notifyMaxPriority:mainCollection];
   }
